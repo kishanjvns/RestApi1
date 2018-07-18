88765b99a2c3dfc04c2a4932f5858b8c4a26ab41
kishan <kishanjvns@hotmail.com>
2018-07-18 23:01:50 +0530

    this is second commit

--- src/main/java/com/example/demo/model/BaseEntity.java
+++ src/main/java/com/example/demo/model/BaseEntity.java
@@ -1,13 +1,52 @@
 package com.example.demo.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 	@Id
-	
-	private int id;
-	
-	
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+
+	@Column(name = "created_time")
+	@CreationTimestamp
+	private Timestamp createdTime;
+	@Column(name = "upadted_time")
+	@UpdateTimestamp
+	private Timestamp updatedTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Timestamp getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Timestamp createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public Timestamp getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Timestamp updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
 }
--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,5 +1,67 @@
 package com.example.demo.model;
 
-public class Customer {
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Customer")
+public class Customer extends BaseEntity{
+	private String firstName;
+	private String LastName;
+	@OneToMany(targetEntity=Contact.class)
+	@JoinColumn(referencedColumnName="id",name="customer_id")
+	private Set<Contact> contacts;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public Set<Contact> getContacts() {
+		return contacts;
+	}
+	public void setContacts(Set<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((LastName == null) ? 0 : LastName.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (LastName == null) {
+			if (other.LastName != null)
+				return false;
+		} else if (!LastName.equals(other.LastName))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		return true;
+	}
+	
+	
 }
