eb270558109990cf79bf7e4a60010345619e7597
kishan <kishanjvns@hotmail.com>
2018-07-18 21:00:00 +0530

    first commit

---
+++ src/main/java/com/example/demo/RestApi1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApi1Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+	@Id
+	
+	private int id;
+	
+	
+}
